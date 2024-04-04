5a398a82e1fb876d1f361b2e38ebc62c04a657cc
sebastian escorcia <sebastianescorcia18@gmail.com>
2024-04-04 13:22:00 -0500

    Agregar archivos de proyecto.

---
+++ Entity/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Persona
+    {
+        public Persona(string nombre,string identifiacion, string sexo, int edad )
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Sexo = sexo;
+            Identificacion = identifiacion;
+            Edad = edad;
+        }
+        public Persona()
+        {
+            
+        }
+        public string Nombre { get; set; }
+        public int Edad {  get; set; }
+        public string Sexo { get; set; }
+        public string Identificacion { get; set; }
+        public string Pulsacion { get; set; }
+
+    }
+}
